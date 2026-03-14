07e40d89b0956b082e1112351586a6dc1f07bba1
grahamm-debug <frightencantor@gmail.com>
2026-03-14 18:55:01 +0800

    Delete bicol-notices-hub-main directory

--- bicol-notices-hub-main/src/pages/student/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as StudentDashboardPage } from './StudentDashboardPage';
-export { default as AllNoticesPage } from './AllNoticesPage';
-export { default as UrgentNoticesPage } from './UrgentNoticesPage';
-export { default as DepartmentNoticesPage } from './DepartmentNoticesPage';
-export { default as ReadTrackingPage } from './ReadTrackingPage';
-export { default as StudentProfilePage } from './StudentProfilePage';
-
